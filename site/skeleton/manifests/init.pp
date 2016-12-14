d7f2afdd5bf900278b2b4fa45e8b73744cc41dcb
Reidv <reidvacek@hotmail.com>
2016-12-14 13:34:27 -0600

    /etc/skel/.bashrc

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -9,5 +9,11 @@ class skeleton {
   
   # add a resource to manage /etc/skel/.bashrc
 
-
+file { '/etc/skel/.bashrc':
+    ensure => file,
+    source => 'puppet:///modules/skeleton/bashrc'
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
 }
