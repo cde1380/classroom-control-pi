cf2a1b3399d9d81a242787a847539abf095dbf39
smanekia <siddybob69@gmail.com>
2016-12-14 14:52:22 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -31,6 +31,11 @@
 unless $environment in [ 'production', 'staging' ] {
 notify { "Warning: this is a development environment on ${::fqdn}": } }
 # ...
+node 'training.puppetlabs.vm' {
+#notify { "This will only be enforced on the Linux container.": }
+exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd": path => '/usr/bin:/usr/local/bin',
+creates => '/etc/motd',
+} }
 }
 
 
