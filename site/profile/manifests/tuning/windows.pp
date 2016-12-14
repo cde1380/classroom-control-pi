01b68e7141bd929947acd88756a23c901cee72fa
Laura Macchi <lau.macchi@gmail.com>
2016-12-14 14:38:48 -0600

    Fix titles to avoid duplicate resources

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -5,13 +5,13 @@ class profile::tuning::windows {
       data   => '1',
     }
 
-    registry::value { 'KeepAliveInterval':
+    registry::value { 'TcpMaxDataRetransmissions':
       key    => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
       value  => 'TcpMaxDataRetransmissions',
       data   => '5',
     }
     
-    registry::value { 'KeepAliveInterval':
+    registry::value { 'TcpTimedWaitDelay':
       key    => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
       value  => 'TcpTimedWaitDelay',
       data   => '0x0000001e',
