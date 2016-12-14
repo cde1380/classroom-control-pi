40d607d9dfcffece9efb264ddf794720e32d074c
Reidv <reidvacek@hotmail.com>
2016-12-14 13:40:22 -0600

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -12,8 +12,5 @@ class skeleton {
 file { '/etc/skel/.bashrc':
     ensure => file,
     source => 'puppet:///modules/skeleton/bashrc',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
   }
 }
