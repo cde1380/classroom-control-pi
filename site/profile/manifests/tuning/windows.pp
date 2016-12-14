4fdf8cc83ba030094147386b9f57ca0388b76cd4
Laura Macchi <lau.macchi@gmail.com>
2016-12-14 14:35:13 -0600

    Add registry values

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -5,6 +5,16 @@ class profile::tuning::windows {
       data   => '1',
     }
 
-  # Add more registry resources as directed
+    registry::value { 'KeepAliveInterval':
+      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+      value  => 'TcpMaxDataRetransmissions',
+      data   => '5',
+    }
+    
+    registry::value { 'KeepAliveInterval':
+      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
+      value  => 'TcpTimedWaitDelay',
+      data   => '0x0000001e',
+    }
   
 }
