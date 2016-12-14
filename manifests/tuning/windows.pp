74d5cdb6473b5058bebae263582c51d566ea021d
mattslater <matthew.jj.slater@gmail.com>
2016-12-14 20:34:52 +0000

    tuning windows

---
+++ manifests/tuning/windows.pp
@@ -0,0 +1,17 @@
+class profile::tuning::windows {
+registry::value { 'KeepAliveInterval':
+key => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
+value => 'KeepAliveInterval',
+data => '1',
+}
+registry::value { 'TcpMaxDataRetransmission':
+key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+value => 'TcpMaxDataRetransmissions',
+data => '5',
+}
+registry::value { 'TcpTimedWaitDelay':
+key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+value => 'TcpTimedWaitDelay',
+data => '0x0000001e',
+}
+}
