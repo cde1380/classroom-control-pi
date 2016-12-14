deda8c172610389a2c66fbb3f777dda177fb77fa
mattslater <matthew.jj.slater@gmail.com>
2016-12-14 20:59:01 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,10 +39,10 @@ node mattslater.puppetlabs.vm {
   # notify { "Warning: this is a development environment on ${::fqdn}": }
   #} 
   #notify { "This will only be enforced on the Linux container.": }
-  #exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
-  #  path => '/usr/bin:/usr/local/bin',
-  #  creates => '/etc/motd'
-  #}
+  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+    path => '/usr/bin:/usr/local/bin',
+    creates => '/etc/motd'
+  }
   include profile::redis
   #notify {"System uptime:  -=- Days: ${::system_uptime['days']} -=- Hours: ${::system_uptime['hours']}" }
 }
