8bd81ff8433d95577818e2487b62ecd872332786
apocasan <apocasan@videotron.ca>
2016-12-14 14:36:26 -0500

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,12 +1,18 @@
 class skeleton {
   file { '/etc/skel':
-#    ensure => ???,   # what value should go here?
+    ensure => directory,   # what value should go here?
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
   }
   
   # add a resource to manage /etc/skel/.bashrc
-
-
+  file { '/etc/skel/.bashrc':
+    ensure => file,   # what value should go here?
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => "puppet:///modules/skel/bashrc",
+  }
+ 
 }
