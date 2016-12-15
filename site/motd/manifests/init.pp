8933a5e9fa0f44b303f1ed9cca55b8210bd32a0c
mattslater <matthew.jj.slater@gmail.com>
2016-12-15 14:13:30 +0000

    Update init.pp

--- site/motd/manifests/init.pp
+++ site/motd/manifests/init.pp
@@ -1,12 +1,10 @@
 class motd {
-  exec { "figlet 'Welcome to ${::fqdn}!' > /etc/motd":
-    path    => '/bin:/usr/bin:/usr/local/bin',
-    creates => '/etc/motd',
-    # what relationship should we add here to ensure that figlet is available to run?
-    
-  }
-
-  package { 'figlet':
-    ensure => present,
-  }
+exec { "figlet 'Welcome to ${::fqdn}!' > /etc/motd":
+path => '/bin:/usr/bin:/usr/local/bin',
+creates => '/etc/motd',
+require => Package['figlet'],
+}
+package { 'figlet':
+ensure => present,
+}
 }
